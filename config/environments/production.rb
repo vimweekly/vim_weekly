6f4eddc4a8b9d04a829713cfd7a0668983158c9d
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2014-01-19 00:27:51 -0500

    fix heroku images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ VimWeekly::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # last hoop that needs to be jumped to make heroku work
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 end
