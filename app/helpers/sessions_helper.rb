cd913ad6fe20d2c29924a4df85c4430e9a185db1
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2014-01-25 22:56:11 -0500

    Include partials and also fixed bunch of bugs

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+
+  def sign_in(admin)
+    cookies.permanent[:admin_id] = admin.id
+    self.current_admin = admin
+  end
+
+  def current_admin=(admin)
+    @current_admin = admin 
+  end
+
+  def current_admin
+    @current_admin ||= Admin.find_by_id(cookies[:admin_id])
+  end
+
+  def signed_in?
+    !current_admin.nil?
+  end
+
+  def sign_out
+    self.current_admin = nil
+    cookies.delete(:admin_id)
+  end
 end
