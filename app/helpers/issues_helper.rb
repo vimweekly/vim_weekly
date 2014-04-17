dfea32aaa43993a6391c812d4bc9a33d742d3d66
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2014-04-16 21:14:11 -0400

    better xml parsing

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,4 +1,8 @@
 module IssuesHelper
+        #<summary><%= replace_special_chars val.summary.to_s %></summary>
+        #<pubDate><%= Date.parse(val['release_date']).rfc2822%></pubDate>
+        #<code><%= replace_special_chars val.code.to_s %></code>
+        #<description><%= replace_special_chars val.description.to_s %></description>
   def replace_special_chars input
     final = ""
     input.each_char do |x|
